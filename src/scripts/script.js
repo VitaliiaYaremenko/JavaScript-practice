f78a7e43e70e1a990aa7ee3ef6235385f6515192
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-08-09 23:01:22 +0200

    completed new branch

---
+++ src/scripts/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let admin;
+let name;
+
+name = "Джон";
+admin = name;
+
+alert(admin);
+//________________________________________________
+
+const ourPlanetName = "Земля";
+
+const currentUserName = "user";
