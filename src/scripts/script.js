270f5269d301b8380d344b5aa9c88812c28180eb
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-08-10 23:56:40 +0200

    completed 6 branch

---
+++ src/scripts/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const result1 = 5 > 4; // true
+const result2 = "ананас" > "яблоко"; //false
+const result3 = "2" > "12"; // true
+const result4 = undefined == null; // true
+const result5 = undefined === null; //false
+const result6 = null == "\n0\n"; // false
+const result7 = null === +"\n0\n"; // false
