63d9ed8a1aa4d6018d94478159aff55341792ecf
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-08-18 23:49:58 +0200

    added changes

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,12 +3,36 @@
 const styles = ["Джаз", "Блюз"];
 styles.push("Рок-н-ролл");
 
-styles[1] = "Классика";
+const averageArrayLength = Math.floor(styles.length - 1) / 2;
+console.log(averageArrayLength);
+styles[averageArrayLength] = "Классика";
 
 const deleteElem = styles.shift();
 console.log(deleteElem);
 
 styles.unshift("Рэп", "Регги");
 console.log(styles);
+
+//___________________________________________
+
+const sumInput = () => {
+  let result = [];
+
+  while (true) {
+    let userNumber = prompt("Enter your number: ", "0");
+
+    if (userNumber === null || userNumber === " ") break;
+    result.push(+userNumber);
+  }
+
+  let sum = 0;
+
+  for (let number of result) {
+    sum += number;
+  }
+  return sum;
+};
+
+console.log(sumInput());
 
 //___________________________________________
